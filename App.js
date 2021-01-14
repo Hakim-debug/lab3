2dcb51e3d9a2c827576380fad6065bf8bf921ae3
Hakim Laoukili <hakimlaoukili@gmail.com>
2021-01-14 01:00:22 +0100

    Update Text
    Edit som new text

--- App.js
+++ App.js
@@ -52,7 +52,7 @@ export default function App() {
       <View style={{ flexDirection: "row" }}>
         <Text>Hello {value}!</Text>
         <Image
-          style={{ height: 150, width: 150 }}
+          style={{ height: 150, width: 250 }}
           source={require("./assets/watch.jpg")}
         ></Image>
       </View>
@@ -73,10 +73,10 @@ export default function App() {
         <View
           style={{
             backgroundColor: "yellow",
-            marginLeft: 25,
+            marginLeft: 0,
             marginTop: 50,
             width: "100%",
-            height: "25%",
+            height: "100%",
           }}
         >
           <Text>Please Enter Your Name:</Text>
@@ -95,7 +95,7 @@ export default function App() {
           </TouchableOpacity>
         </View>
       </Modal>
-
+      <Text style={{ fontWeight: "bold" }}>Today's Highlights</Text>
       <ScrollView style={{ marginTop: 50 }}>
         <Text>
           Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin.
@@ -105,27 +105,14 @@ export default function App() {
           århundraden, utan även övergången till elektronisk typografi utan
           större förändringar. Det blev allmänt känt på 1960-talet i samband med
           lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med
-          mjukvaror som Aldus PageMaker. Lorem Ipsum är en utfyllnadstext från
-          tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan
-          1500-talet, när en okänd boksättare tog att antal bokstäver och
-          blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har
-          inte bara överlevt fem århundraden, utan även övergången till
-          elektronisk typografi utan större förändringar. Det blev allmänt känt
-          på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av
-          Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.
+          mjukvaror som Aldus PageMaker.
         </Text>
 
         <Text>
           I motsättning till vad många tror, är inte Lorem Ipsum slumpvisa ord.
           Det har sina rötter i ett stycke klassiskt litteratur på latin från 45
           år före år 0, och är alltså över 2000 år gammalt. Richard McClintock,
-          en professor i latin på Hampden-Sydney College i Virginia, översatte
-          ett av de mer ovanliga orden, consectetur, från ett stycke Lorem Ipsum
-          och fann dess ursprung genom att studera användningen av dessa ord i
-          klassisk litteratur. Lorem Ipsum kommer från styckena 1.10.32 och
-          1.10.33 av "de Finibus Bonorum et Malorum" (Ytterligheterna av ont och
-          gott) av Cicero, skriven 45 före år 0. Boken är en avhandling i
-          teorier om etik, och var väldigt populär under renäsanssen. Den
+          en professor i latin på Hampden-Sydney College i Virginia. Den
           inledande meningen i Lorem Ipsum, "Lorem Ipsum dolor sit amet...",
           kommer från stycke 1.10.32. Lorem Ipsum är en utfyllnadstext från
           tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan
@@ -159,6 +146,7 @@ export default function App() {
           marginLeft: 25,
           marginTop: 20,
           flexDirection: "row",
+          backgroundColor: "white",
         }}
       >
         <TouchableOpacity onPress={() => createTwoButtonAlert()}>
